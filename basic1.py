c12da4eceb3419e2af96fc530c23d4b78afcbc0b
unknown <fengchuanyun85@163.com>
2019-05-07 10:05:25 +0800

    201905071005 commit

--- basic1.py
+++ basic1.py
@@ -32,11 +32,13 @@ print(s3)
 print(s4)
 print(s5)
 print(s6)
+
 # str()函数，把其他类型的转为字符串
 a = 20
 print(type(a))
 a = str(a)
 print(type(a))
+
 # 三单引号/三双引号可以用来做注释，多行注释
 '''
 这是三单引号，
@@ -48,18 +50,38 @@ print(type(a))
 也是注释，
 也不会被执行
 """
+
+# 字符串的拼接
+a = 'hello'
+b = ' world'
+c = a + b
+print(c)
+
+# 字符串的分割
+s = 'hello_world_jerry'
+s1 = s.split('_')
+print('s1=', s1)
+
 # 字符串中的特殊字符
 # \n 换行  \r 回车
 print('i\nlove\nu')
+
 # 在特殊字符前加\，会把特殊字符转义掉
 print('i\\nlove\\nu')
+
 # 字符串格式化操作，主要有4种：%s  %d   %f   %.2f
 name = 'tom'
 age = 18
 f1 = 1.85
 print('%s is %d years old' % (name, age))
 print('身高为%f，保留2位小数是%.2f' % (f1, f1))
+
 # 字符串的索引
+'''
+h    e    l     l    o
+0    1    2     3    4
+-5   -4  -3    -2   -1
+'''
 s = 'hello'
 print(s[0])
 print(s[-1])
@@ -83,14 +105,17 @@ print(s3.find('o'))
 print(s3.index('o'))
 print(s3.find('p'))
 # print(s3.index('p'))
+
 # 字符串的方法--替换
 print(s3.replace('l', 'L'))
+
 # 字符串的方法--大小写
 s1 = 'heLLo'
 if s1.lower() == 'hello':
     print('登录成功')
 if s1.upper() == 'HELLO':
     print('登录成功')
+
 # 字符串的方法--判断
 s = 'helloworld123'
 print(s.isdigit())
@@ -101,22 +126,29 @@ print(s.isupper())
 print(s.startswith('h'))
 print(s.endswith('d'))
 
+# input() ------用户输入的任何内容，都当做字符串
+name = input('请输入您的姓名：')
+print(name)
 
 # 变量------------------------------------------------------------------------------------------------------
 # 赋值的3种方式
 name = 'jerry'            # 传统赋值
 name1 = name2 = 'tom'     # 链式赋值
 name3, age = 'jerry', 18  # 序列解包赋值
+
 # 查看变量的类型
 a = '34'
 print(type(a))
+
 # 查看地址
 name = user = 'Tom'
 print(id(name))
 print(id(user))
+
 # 关键字列表
 import keyword
 print(keyword.kwlist)
+
 # 推荐命名方法
 student_name_list = ['Tom', 'Jerry', 'Lucy']
 
