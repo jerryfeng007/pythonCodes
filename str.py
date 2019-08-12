a0dc7c9309de2a918ee51e786e01234d38052072
unknown <fengchuanyun85@163.com>
2019-08-12 14:01:38 +0800

    20190812 1401 提交

--- str.py
+++ str.py
@@ -1,15 +1,21 @@
-# 字符串的定义：单引号、双引号、三单引号、三双引号
-# 三单引号、三i双引号可以写多行
+print('-----------------单引号、双引号、三单引号、三双引号---------------------------------------')
+
+# 单行
 s1 = 'i love u'
 s2 = "i love u"
+
+# 多行
 s3 = '''
 i love u
 Do u love me?
 '''
+
 s4 = """
 i love u
 i love him!
 """
+
+# 引号嵌套
 s5 = "i'm a teacher"
 s6 = 'the "name" is jerry'
 print(s1)
@@ -19,55 +25,59 @@ print(s4)
 print(s5)
 print(s6)
 
-# str()函数，把其他类型的转为字符串
+print('------------------------------------------------str()强制转换函数---------------------------------------')
+
+# 把其他类型转为字符串
 a = 20
-print(type(a))
 a = str(a)
 print(type(a))
 
-# 三单引号/三双引号可以用来做注释，多行注释
+print('------------------------------------------------多行注释---------------------------------------')
+
 '''
-这是三单引号，
-这是注释，
-不会被执行
+a = 'hello'
+b = ' world'
+c = a + b
+print(c)
 '''
+
 """
-这是三双引号，
-也是注释，
-也不会被执行
+a = 'hello'
+b = ' world'
+c = a + b
+print(c)
 """
 
-# 字符串的拼接
+print('------------------------------------------------字符串的拼接-------------------------------------')
+
 a = 'hello'
 b = ' world'
 c = a + b
 print(c)
 
-# 字符串的分割
-s = 'hello_world_jerry'
-s1 = s.split('_')
-print('s1=', s1)
-s = 'hello world jerry'
-s1 = s.split(' ')
-print('s1=', s1)
+print('------------------------------------------------字符串中的特殊字符-------------------------------------')
 
-# 字符串中的特殊字符
-# \n 换行  \r 回车
+# \n 换行
+# \r 回车
+# \t tab
 print('i\nlove\nu')
 
 # 在特殊字符前加\，会把特殊字符转义掉
 print('i\\nlove\\nu')
 
-# 字符串格式化操作，主要有4种：%s  %d   %f   %.2f
+print('------------------------------------------------字符串格式化-------------------------------------')
+
+# %s  %d   %f   %.2f
 name = 'tom'
 age = 18
-f1 = 1.85
+height = 1.85
 print('%s is %d years old' % (name, age))
-print('身高为%f，保留2位小数是%.2f' % (f1, f1))
 print('{} is {} years old'.format(name, age))
 print(f'{name} is {age} years old')
+print('身高为%f，保留2位小数是%.2f' % (height, height))
 
-# 字符串的索引
+print('------------------------------------------------字符串的索引-------------------------------------')
+
 '''
 h    e    l     l    o
 0    1    2     3    4
@@ -84,61 +94,95 @@ print(s[4:1:-1])  # -代表从右往左，从4开始，取的1的前一位
 print(s[-1:-4:-1])  # -代表从右往左，从-1开始，取的-4的前一位
 print(s[4:1:1])  # 空，因为取不到
 
-# 字符串的方法--strip()  默认去除两边的空格，去除内容可以指定
+print('------------------------------------------------字符串的方法-------------------------------------')
+
+# strip()
+# 默认去除两边的空格，去除内容可以指定
 # s1 = input('请输入一个数字：').strip()
 # print(s1)
-s2 = '   hello   '.strip()
-print(s2)
 
-# 字符串的方法--查找
-# find 和 index功能类似，都是返回要找的字符的索引，区别是如果找不到时，find返回-1，index会报错
+s12 = '   hello   '
+s12 = s12 .strip()
+print(s12)
+
+# 分割
+s = 'hello_world_jerry'
+s1 = s.split('_')
+print('s1=', s1)  # 分割后是个列表
+
+s = 'hello world jerry'
+s1 = s.split(' ', 1)  # 只分割1次
+print('s1=', s1)
+
+# 拼接
+l = ['a', 'b', 'c', 'd']
+s = '_'.join(l)
+print(s)
+
+# 查找
+# find 和 index功能类似，都是返回要找的字符的索引
+# 区别是如果找不到时，find返回-1，index会报错
 s3 = 'hello'
-print(s3.count('l'))
+# find
 print(s3.find('o'))
-print(s3.index('o'))
 print(s3.find('p'))
+
+# index
+print(s3.index('o'))
 # print(s3.index('p'))
 
-# 字符串的方法--替换
-print(s3.replace('l', 'L'))
+# 统计
+print(s3.count('l'))
 
-# 字符串的方法--大小写
+# 长度
+s = 'jerry'
+print(len(s))
+
+# 替换
+s4 = s3.replace('l', 'L')
+print(s4)
+
+# 大小写
 s1 = 'heLLo'
 if s1.lower() == 'hello':
     print('登录成功')
 if s1.upper() == 'HELLO':
     print('登录成功')
 
-# 字符串的方法--判断
+print('------------------------------------------------字符串的判断-------------------------------------')
+
+# 判断
 s = 'helloworld123'
 print(s.isdigit())
 print(s.isalpha())
-print(s.isalnum())
+print(s.isalnum())  # 或字母、或数字
 print(s.islower())
 print(s.isupper())
 print(s.startswith('h'))
 print(s.endswith('d'))
 print(s.isspace())
 
-# 字符串的编码、解码
+print('------------------------------------------------字符串编码-------------------------------------')
+
 # ASCII码
 print(ord('A'))
 print(chr(65))
 
+# 编码
 s = '马云非'
 s1 = s.encode('gbk')
 s2 = s.encode('utf8')
 print(s1)
 print(s2)
+
+# 解码
 s3 = s1.decode('gbk')
 s4 = s2.decode('utf8')
 print(s3)
 print(s4)
 
-# 字符串长度
-s = 'jerry'
-print(len(s))
+print('--------------------------------------------input函数输入的都为字符串-------------------------------------')
 
-# input() ------用户输入的任何内容，都当做字符串
-# name = input('请输入您的姓名：')
-# print(name)
+# input函数输入的任何内容，都当做字符串
+# num = input('请输入一个数：')
+# print(type(num))  # str类型
