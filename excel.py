3c935125713f3004cf61527a1f57333bad9e8f52
jerry <fengchuanyun85@163.com>
2019-09-01 09:09:16 +0800

    20190901 0909

--- excel.py
+++ excel.py
@@ -1,56 +1,65 @@
 import openpyxl
-import time,os
-from openpyxl.styles import Font, colors, Alignment  #这里有个规则，不能是平级
-os.chdir('D:\\learnPY\\excel')
-path='excel_demo.xlsx'
+from openpyxl.styles import Font, colors, Alignment
+
+print('--------------------------------------openpyxl操作excel-------------------------------------')
+
+# 打开excel文件
+wb = openpyxl.load_workbook('excel_test.xlsx')  # 重要1
+
+# 获取所有工作表的列表
+sheets = wb.sheetnames
+print(sheets)
+
+# 获取某个工作表的对象，供后续调用其它方法使用  # 重要2
+sheet2 = wb['Sheet2']
+
+# 获取最大行数、列数
+print('获取最大列数：', sheet2.max_column)
+print('获取最大行数：', sheet2.max_row)
+
+# 设置字体颜色
+font = Font(color=colors.RED)
+alignment = Alignment(horizontal='general', vertical='center')
+
+sheet2['B2'].font = font
+sheet2['B2'].alignment = alignment
+
+print('----------------------------写入数据到excel-------------------------------------------')
+# 写入单条
+sheet2['A9'].value = '美元'                           # .value可以省略
+sheet2.cell(row=8, column=1).value = '我是一个好人'   # 推荐
 
 
-#打开excel文件
-wb=openpyxl.load_workbook(path)
-
-ss=wb.get_sheet_names()
-print('获取工作簿中的所有所有工作表名：',ss)
-
-s1=wb.get_sheet_names()[0]
-print('方法1：获取某个工作表名',s1)
-
-s2=wb.get_sheet_by_name('Sheet1')
-print('方法2：获取某个工作表名的对象，供后续调用其它方法使用',s2)
+# for循环写入
+def aaa(name, age):
+    for i in range(10, 15):
+        sheet2.cell(row=i, column=1).value = name[i-10]
+        sheet2.cell(row=i, column=2).value = age[i - 10]
 
 
-#获取单元格数据
-print('方法1：获取单元格A1的值',s2['A1'].value)
+name = ['jerry', 'tom', 'lucy', 'lily', 'howard']
+age = [18, 19, 20, 21, 22]
+aaa(name, age)
 
-#这个写法的好处是方便用for循环取值
-print('方法2：获取单元格A2的值',s2.cell(row=2,column=1).value)
+print('-------------------------获取单元格数据------------------------------------------------')
+# 获取某个单元格
+print(sheet2['A1'].value)
+print(sheet2.cell(row=1, column=2).value)
 
-#写入数据到excel
-s2.cell(row=5,column=1).value='我是一个好人'
-s2['B5'].value='你呢'  #此处也可以不用value
+# for循环输出所有单元格
+for i in range(1, sheet2.max_row+1):
+    for j in range(1, sheet2.max_column+1):
+        print(sheet2.cell(row=i, column=j).value, end='\t')
+    print()
 
+print('---------------------遍历excel(模拟遍历测试用例)---------------------------------------------')
+for i in range(2, sheet2.max_row+1):
+    book = sheet2.cell(row=i, column=1).value
+    major = sheet2.cell(row=i, column=2).value
+    student = sheet2.cell(row=i, column=3).value
+    score = sheet2.cell(row=i, column=4).value
 
-print('获取最大列数：',s2.max_column)
-print('获取最大行数：',s2.max_row)
+    print(book, major, student, score)
 
-
-#循环输出单元格里的数据
-for i in range(1,s2.max_row+1):
-    for j in range(1,s2.max_column+1):
-        print(s2.cell(row=i,column=j).value,'\t',end='')  #\t，是Tab，end=''表示不换行
-    print() #表示换行
-
-
-#设置字体颜色
-myfont=Font(color=colors.RED)
-s2['B2'].font=myfont
-
-
-
-#写入数据到excel
-s2.cell(row=6,column=1).value='黄继光'
-s2['B6'].value='哪咤'  #此处也可以不用value
-
-
-#如果写入了数据，记得一定要保存
-wb.save(path)
-
+# 记得一定要保存
+wb.save('excel_test.xlsx')
