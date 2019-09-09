f3ecaa215bc0a80beadf1722ab45e675354a43ab
jerryfeng007 <44737764+jerryfeng007@users.noreply.github.com>
2019-09-09 22:16:38 +0800

    Update latest_news.py

--- monitor/latest_news.py
+++ monitor/latest_news.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-# 从金融界网站获取7x24小时最新一条新闻的标题
+# 从网站获取7x24小时最新一条新闻的标题
 url = 'http://finance.jrj.com.cn/yaowen'
 # res = requests.get(url).content.decode('gbk', 'ignore').encode('utf8', 'ignore')
 # res = requests.get(url).content
@@ -28,10 +28,10 @@ print(time_web)
 # 从数据库查询出7x24小时的最新一条新闻的标题
 # Mongo数据库配置和连接
 try:
-    with MongoClient('10.99.4.25', 27017) as client:
-        db = client['z3dbus']
-        db.authenticate('z3dbusread', 'z3dbusread')
-        print('连接mongo生产数据库成功!')
+    with MongoClient('108.9.91.76', 3306) as client:
+        db = client['z3']
+        db.authenticate('z3dad', 'd')
+        print('连接数据库成功!')
 
         # 查询7x24小时最新的一条
         collection = db['Z3_NEWS']
@@ -51,9 +51,9 @@ except Exception as e:
 
 # 当网站上的最新新闻的时间大于客户端最新新闻的时间时，给数据人员发消息
 if time_web > time_mongo:
-    params = {'action': 'send_msg', 'users': 'chuanyun.feng|tao.liu', 'content':
+    params = {'action': 'send_msg', 'users': 'xxx|yyy', 'content':
               '智胜生产环境7x24小时新闻监控：' + '\n' +
-              'mongo库Z3_NEWS表查询出来的7x24小时最新新闻与金融界网站获取到的不一致！' + '\n' +
+              'mongo库Z3_NEWS表查询出来的7x24小时最新新闻与网站获取到的不一致！' + '\n' +
               '从数据库查询到的最新新闻为：' + title_mongo + '，时间为：' + time_mongo + '\n' +
               '金融界网站获取的最新新闻为：' + title_web + '，时间为：' + time_web}
-    requests.get('http://wx.jrj.com.cn/api/weixin.jsp', params=params)
+    requests.get('http://wx.nn.com.cn/api/weixin.jsp', params=params)
