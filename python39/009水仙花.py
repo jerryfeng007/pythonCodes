7848f7992f0806b582f3600beec38eedcaba523e
jerry <fengchuanyun85@163.com>
2019-10-06 23:10:26 +0800

    1006

--- python39/009水仙花.py
+++ python39/009水仙花.py
@@ -8,3 +8,13 @@ for i in range(1, 100000):
         sum += int(j) ** l
     if sum == i:
         print(i)
+
+
+# 方法2
+for i in range(100, 1000):
+    bai = i // 100
+    shi = i // 10 % 10
+    ge = i % 10
+    if bai ** 3 + shi ** 3 + ge ** 3 == i:
+        print(i)
+
