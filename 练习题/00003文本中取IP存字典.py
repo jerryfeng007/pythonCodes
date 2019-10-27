8d5637195e4cbef4836bf9f11421e52740a4a644
jerry <fengchuanyun85@163.com>
2019-10-27 12:00:39 +0800

    00003

--- 练习题/00003文本中取IP存字典.py
+++ 练习题/00003文本中取IP存字典.py
@@ -1,7 +1,7 @@
 # 从文本中取出ip，并计算每个ip的次数
 
 d = {}
-with open('00003文本取IP', 'r', encoding='utf8') as f:
+with open('00003文本取IP存字典', 'r', encoding='utf8') as f:
     for line in f:
         ip = line.split('，')[0]
         if ip not in d:
